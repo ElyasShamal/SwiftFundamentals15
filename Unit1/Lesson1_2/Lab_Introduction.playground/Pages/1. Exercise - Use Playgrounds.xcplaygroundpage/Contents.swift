3e4ea72d6c03fb01f99148e725d641813d954c4b
Elyas Shamal <Elyas.shamal2@gmail.com>
2025-10-22 17:15:36 -0400

    Completed Lab 1-2

--- Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,12 +10,17 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
+print("Song Title: Shape of you by Ed Sheeren")
 
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("Take my hand, stop, put Van man on the jukebox")
+print("And then we start to dance, and now i'm singing like")
+print("Girl, you know i want your love")
+print("Your love was handmade for somebody like me")
+print("Come on now, follow my lead I may be crazy don't mind me")
 
 
 /*:
