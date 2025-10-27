da69a8e2ccc1d235f3bd67f4805565ee2d610304
Elyas Shamal <Elyas.shamal2@gmail.com>
2025-10-26 23:11:37 -0400

    Completed lab 1 - 5

--- Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
@@ -11,6 +11,18 @@ let targetLowerBound = 120
 let targetUpperBound = 150
 let currentHR = 147
 
+let isIntarget = currentHR >= targetLowerBound && currentHR <= targetUpperBound
+let isBelowTarget = currentHR < targetLowerBound
+let isAboveTarget = currentHR > targetUpperBound
+
+if isIntarget {
+    print("You're right on truck!")
+}else if isBelowTarget {
+    print("You'r doing great, but try to push it a bit!")
+}else if isAboveTarget {
+    print("You're on fire! Slow it down just a bit.")
+}
+
 /*:
 [Previous](@previous)  |  page 5 of 9  |  [Next: Exercise - Switch Statements](@next)
  */
